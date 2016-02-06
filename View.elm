54f97c8d8f3988e422ac642058fd4942479a0699
asweingarten <asweingarten+github@gmail.com>
2016-02-06 14:48:24 -0800

    Refactor View; Sandy to investigate error

--- View.elm
+++ View.elm
@@ -3,17 +3,28 @@ module View where
 import Types exposing (..)
 import Model exposing (..)
 
-import Graphics.Element exposing (Element (..), show, flow, right, centered, spacer)
+import Graphics.Element exposing (Element (..), show, flow, right, down, centered, spacer)
 import Graphics.Collage exposing (Form (..), collage, toForm, text)
-import Html exposing (div, button, span, input)
-import Html.Events exposing (onClick, on, targetValue)
-import Html.Attributes exposing (class, style)
+import Graphics.Input.Field exposing (field, noContent, defaultStyle)
 import ParseInt exposing (parseInt)
 import List exposing (concat)
 import Array exposing (fromList, get, length, map, toList)
 import Color exposing (red)
 import Text exposing (Text (..), fromString, color)
 
+view : Signal.Address Action -> Model -> Element
+view address model =
+    flow down [viewChords model, showUi address]
+    
+showUi : Signal.Address Action -> Element
+showUi address  = 
+    let onBpmChange s = (case parseInt s.string of
+            Ok newBpm -> SetTempo newBpm
+            Err _     -> SetTempo 1
+        ) |> Signal.message address
+    in field defaultStyle onBpmChange "BPM" noContent
+    
+    
 redText : Int -> Tone -> List Element
 redText index chord =
     let selected = get index tones
@@ -22,20 +33,7 @@ redText index chord =
         label = toString chord |> fromString |> styling |> centered
     in [label, spacer 10 1]
 
-view : Signal.Address Action -> Model -> Element
-view address (index, time, bpm) =
-    let buttonJob action string = button [ onClick address action ] [ Html.text string ]
-
-        onBmpChange s = (case parseInt s of
-            Ok newBpm -> SetTempo newBpm
-            Err _     -> SetTempo bpm
-        ) |> Signal.message address
-
-        elems = [ buttonJob Decrement "-"
-                , buttonJob Increment "+"
-                , input [ on "input" targetValue onBmpChange ] []
-                ]
-
-        layout = flow right (toList (map (\tone -> redText index tone) tones) |> concat)
-
+viewChords : Model -> Element
+viewChords (index, time, bpm) =
+    let layout = flow right (toList (map (\tone -> redText index tone) tones) |> concat)
     in [toForm layout] |> collage 800 200
