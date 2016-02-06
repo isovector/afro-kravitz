81a134d62740f10dddd0a6d27ebf1d022a07d33b
asweingarten <asweingarten+github@gmail.com>
2016-02-06 15:35:37 -0800

    Center that text box son.

--- View.elm
+++ View.elm
@@ -3,7 +3,7 @@ module View where
 import Types exposing (..)
 import Model exposing (..)
 
-import Graphics.Element exposing (Element (..), show, flow, right, down, centered, spacer)
+import Graphics.Element exposing (Element (..), show, flow, right, down, centered, spacer, container, middle)
 import Graphics.Collage exposing (Form (..), collage, toForm, text)
 import Graphics.Input.Field exposing (field, noContent, defaultStyle, Direction (Forward), Content)
 import ParseInt exposing (parseInt)
@@ -11,6 +11,9 @@ import List exposing (concat)
 import Array exposing (fromList, get, length, map, toList)
 import Color exposing (red)
 import Text exposing (Text (..), fromString, color)
+
+canvasWidth = 800
+canvasHeight = 200
 
 view : Signal.Address Action -> Model -> Content -> Element
 view address model content = (view2 address model) (showUi content)
@@ -24,7 +27,7 @@ bpmMailbox = Signal.mailbox noContent
  
 showUi : Content -> Element
 showUi content  = 
-    (field defaultStyle (Signal.message bpmMailbox.address) "BPM" content)
+    container canvasWidth canvasHeight middle (field defaultStyle (Signal.message bpmMailbox.address) "BPM" content)
     
 bpmSignal : Signal Int
 bpmSignal = let onBpmChange s = (case parseInt s.string of
@@ -42,6 +45,6 @@ redText index chord =
     in [label, spacer 10 1]
 
 viewChords : Model -> Element
-viewChords (index, time, bpm) =
+viewChords (index, time) =
     let layout = flow right (toList (map (\tone -> redText index tone) tones) |> concat)
-    in [toForm layout] |> collage 800 200
+    in [toForm layout] |> collage canvasWidth canvasHeight
