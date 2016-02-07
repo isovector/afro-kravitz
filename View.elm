a16d13a2d51735e70eef5ee4ed994ce72057b464
asweingarten <asweingarten+github@gmail.com>
2016-02-06 16:52:18 -0800

    Label for text field

--- View.elm
+++ View.elm
@@ -3,17 +3,19 @@ module View where
 import Types exposing (..)
 import Model exposing (..)
 
-import Graphics.Element exposing (Element (..), show, flow, right, down, centered, spacer, container, middle)
+import Graphics.Element exposing (Element (..), height, left, beside, midBottom, rightAligned, show, flow, right, down, centered, spacer, container, middle)
 import Graphics.Collage exposing (Form (..), collage, toForm, text)
 import Graphics.Input.Field exposing (field, noContent, defaultStyle, Direction (Forward), Content)
 import ParseInt exposing (parseInt)
-import List exposing (concat)
-import Array exposing (fromList, get, length, map, toList)
+import List exposing (concat, map)
+import Array exposing (fromList, get, length, toList)
 import Color exposing (red)
 import Text exposing (Text (..), fromString, color)
 
 canvasWidth = 800
 canvasHeight = 200
+
+inputHeight = 20
 
 view : Signal.Address Action -> Model -> Content -> Element
 view address model content = (view2 address model) (showUi content)
@@ -27,7 +29,10 @@ bpmMailbox = Signal.mailbox noContent
  
 showUi : Content -> Element
 showUi content  = 
-    container canvasWidth canvasHeight middle (field defaultStyle (Signal.message bpmMailbox.address) "BPM" content)
+    let inputField = field defaultStyle (Signal.message bpmMailbox.address) "BPM" content
+        label = Text.height inputHeight (fromString "Tempo: ") |> rightAligned
+        layout = flow left (map (height inputHeight) [inputField, label])
+    in container canvasWidth canvasHeight midBottom layout
     
 bpmSignal : Signal Int
 bpmSignal = let onBpmChange s = (case parseInt s.string of
@@ -46,5 +51,5 @@ redText index chord =
 
 viewChords : Model -> Element
 viewChords (index, time) =
-    let layout = flow right (toList (map (\tone -> redText index tone) tones) |> concat)
+    let layout = flow right (toList (Array.map (\tone -> redText index tone) tones) |> concat)
     in [toForm layout] |> collage canvasWidth canvasHeight
